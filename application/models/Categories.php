86ad490ec75edfed0204376107899cb3f9abfc37
Geoff McLennan <geoff.d.mclennan@gmail.com>
2018-02-06 21:33:25 -0800

    Added model templates

---
+++ application/models/Categories.php
@@ -0,0 +1,9 @@
+<?php
+
+class Categories extends CI_Model {
+    function __construct() {
+        parent::__construct();
+    }
+}
+
+ ?>
---
+++ application/models/Items.php
@@ -0,0 +1,9 @@
+<?php
+
+class Items extends CI_Model {
+    function __construct() {
+        parent::__construct();
+    }
+}
+
+ ?>
