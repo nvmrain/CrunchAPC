b6737c3f15237d4abac997a3fb66a55feb4f7c9b
Geoff McLennan <geoff.d.mclennan@gmail.com>
2018-02-09 13:11:55 -0800

    Fixed Categories model to use a csv instead of internal object

--- application/models/Categories.php
+++ application/models/Categories.php
@@ -1,37 +1,12 @@
 <?php
 
-class Categories extends CI_Model {
+class Categories extends CSV_Model {
 
-    var $cats = array(
-        '1'     => array('id' => '1', 'name' => 'Processor'),
-        '2'     => array('id' => '2', 'name' => 'RAM'),
-        '3'     => array('id' => '3', 'name' => 'Graphics Card'),
-        '4'     => array('id' => '4', 'name' => 'CPU Fan'),
-        '5'     => array('id' => '5', 'name' => 'Power Source')
-    );
+    public $id;
+    public $name;
 
     function __construct() {
-        parent::__construct();
-    }
-
-    /**
-     * Retrieve and existing DB record.
-     * @param string $key Primary key of the record to return.
-	 * @param string $key2 Second part of composite key, if applicable
-	 * @return object The requested record, null if not found.
-     */
-    function get($key)
-    {
-        return !isset($this->cats[$key]) ? null : $this->cats[$key];
-    }
-
-    /**
-     * Retrieve all existing DB records.
-	 * @return object All records.
-     */
-    function all()
-    {
-        return $this->cats;
+        parent::__construct('../data/Categories.csv', 'id');
     }
 }
 
