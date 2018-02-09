856fdb46a4aa3e19258582b8def1d330b8510287
Geoff McLennan <geoff.d.mclennan@gmail.com>
2018-02-09 13:03:33 -0800

    Added application/core model files, starting data csv files, and Categories, Items, and Sets model files.

--- application/models/Categories.php
+++ application/models/Categories.php
@@ -1,8 +1,37 @@
 <?php
 
 class Categories extends CI_Model {
+
+    var $cats = array(
+        '1'     => array('id' => '1', 'name' => 'Processor'),
+        '2'     => array('id' => '2', 'name' => 'RAM'),
+        '3'     => array('id' => '3', 'name' => 'Graphics Card'),
+        '4'     => array('id' => '4', 'name' => 'CPU Fan'),
+        '5'     => array('id' => '5', 'name' => 'Power Source')
+    );
+
     function __construct() {
         parent::__construct();
+    }
+
+    /**
+     * Retrieve and existing DB record.
+     * @param string $key Primary key of the record to return.
+	 * @param string $key2 Second part of composite key, if applicable
+	 * @return object The requested record, null if not found.
+     */
+    function get($key)
+    {
+        return !isset($this->cats[$key]) ? null : $this->cats[$key];
+    }
+
+    /**
+     * Retrieve all existing DB records.
+	 * @return object All records.
+     */
+    function all()
+    {
+        return $this->cats;
     }
 }
 
--- application/models/Sets.php
+++ application/models/Sets.php
@@ -1,9 +1,28 @@
 <?php
 
-class Sets extends CI_Model {
+class Sets extends CSV_Model {
     function __construct() {
-        parent::__construct();
+        parent::__construct('../data/Sets.csv', 'id');
     }
+
+    /**
+     * id
+     * cpuId
+     * ramId
+     * gpuId
+     * fanId
+     * psuId
+     */
+
+     function get($key1, $key2)
+     {
+         return !isset($this->_data[$key]) ? null : $this->_data[$key];
+     }
+
+     function all()
+     {
+         return $this->_data;
+     }
 }
 
  ?>
