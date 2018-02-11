813661cf29e120852d9c2f291d1eb0eed2cf3967
Xiaoyu <yxy123yu@gmail.com>
2018-02-11 10:15:29 -0800

    finish image style

--- application/views/home.php
+++ application/views/home.php
@@ -11,58 +11,14 @@
 	</div>
 	<div class="col-xs-offset-2"></div>
 </div>
-<!-- <div class="row">
-	<div class="col-xs-4">
-		<select class="form-control">
-			<option>Example 1</option>
-			<option>Example 2</option>
-			<option>Example 3</option>
-			<option>Example 4</option>
-			<option>Example 5</option>
-		</select>
-	</div>
-	<div class="col-xs-offset-2"></div>
+<hr />
+<div class="picture-box" style="">
+	<img src="/assets/images/MotherBoard.png" alt="montherboard" class="montherboard">
+	<img src="/assets/images/Intel-i5.png" alt="CPU i5"  width="170" class="cpu-img">
+	<img src="/assets/images/IntelCooler.png" alt="Cooler" width="180" class="cooler-img">
+	<img src="/assets/images/GTX1080.png" alt="GPU" width="400" class="gpu-img">
+	<img src="/assets/images/PSU500W.png" alt="GPU" width="300" class="psu-img">
+	<img src="/assets/images/3200Hz.png" alt="GPU" width="200" class="memory-img">
+	<!-- <img src="/assets/images/MotherBoard.png"> -->
 </div>
- -->
- <hr />
- <div class="picture-box">
-	<div class="row">
-		<div class="col-xs-4">
-			<img src="" class="img-responsive" alt="image place holder">
-		</div>
-		<div class="col-xs-4">
-			
-			<img src="" class="img-responsive" alt="image place holder">
-		</div>
-		<div class="col-xs-4">
-			
-			<img src="" class="img-responsive" alt="image place holder">
-		</div>
-	</div>
-	<div class="row">
-		<div class="col-xs-4">
-			<img src="" class="img-responsive" alt="image place holder">
-		</div>
-		<div class="col-xs-4">
-			
-			<img src="" class="img-responsive" alt="image place holder">
-		</div>
-		<div class="col-xs-4">
-			
-			<img src="" class="img-responsive" alt="image place holder">
-		</div>
-	</div>
-	<div class="row">
-		<div class="col-xs-4">
-			<img src="" class="img-responsive" alt="image place holder">
-		</div>
-		<div class="col-xs-4">
-			
-			<img src="" class="img-responsive" alt="image place holder">
-		</div>
-		<div class="col-xs-4">
-			
-			<img src="" class="img-responsive" alt="image place holder">
-		</div>
-	</div>
 </div>
