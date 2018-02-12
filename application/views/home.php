991267222ef5bce177a78f07f34b871081e5a00f
Xiaoyu <yxy123yu@gmail.com>
2018-02-11 16:25:04 -0800

    hook catalog page to navbar, and home page now can select build to display

--- application/views/home.php
+++ application/views/home.php
@@ -1,24 +1,17 @@
 <div class="row">
 	<div class="col-xs-4">Select Your Build</div>
 	<div class="col-xs-4">
-		<select class="form-control">
-			<option>Example 1</option>
-			<option>Example 2</option>
-			<option>Example 3</option>
-			<option>Example 4</option>
-			<option>Example 5</option>
+		<select class="form-control" id='selectBuildID'>
+			{options}
 		</select>
 	</div>
-	<div class="col-xs-offset-2"></div>
+	<div class="col-xs-2">
+		<button tyle="submit" class="btn" id="comfirm-select">Submit</button>
+	</div>
 </div>
 <hr />
 <div class="picture-box" style="">
-	<img src="/assets/images/MotherBoard.png" alt="montherboard" class="montherboard">
-	<img src="/assets/images/Intel-i5.png" alt="CPU i5"  width="170" class="cpu-img">
-	<img src="/assets/images/IntelCooler.png" alt="Cooler" width="180" class="cooler-img">
-	<img src="/assets/images/GTX1080.png" alt="GPU" width="400" class="gpu-img">
-	<img src="/assets/images/PSU500W.png" alt="GPU" width="300" class="psu-img">
-	<img src="/assets/images/3200Hz.png" alt="GPU" width="200" class="memory-img">
-	<!-- <img src="/assets/images/MotherBoard.png"> -->
+	
+	{images}
 </div>
 </div>
