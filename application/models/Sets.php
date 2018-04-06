1f17dcd70bf79771ce4045534cc9e31626001cc3
Geoff McLennan <geoff.d.mclennan@gmail.com>
2018-04-05 17:03:31 -0700

    Added property validation for all models

--- application/models/Sets.php
+++ application/models/Sets.php
@@ -19,6 +19,35 @@ class Sets extends CSV_Model {
     function __construct() {
         parent::__construct('../data/Sets.csv', 'id');
     }
+
+	/**
+	 * Crud validation
+	 */
+
+	/**
+	 * Validates that the description is an alphanumeric string
+	 */
+	 function add($record) {
+		 if (!is_string($record->description))
+ 			throw new InvalidArgumentException('Set name must be a string');
+
+ 		$clean = str_replace(" ", "", $record->description);
+ 		if (!ctype_alnum($clean))
+ 			throw new InvalidArgumentException('Set name must be alpha-numeric');
+
+		 parent::add($record);
+	 }
+
+	 function update($record) {
+		 if (!is_string($record->description))
+ 			throw new InvalidArgumentException('Set name must be a string');
+
+ 		$clean = str_replace(" ", "", $record->description);
+ 		if (!ctype_alnum($clean))
+ 			throw new InvalidArgumentException('Set name must be alpha-numeric');
+
+		 parent::update($record);
+	 }
 }
 
  ?>
